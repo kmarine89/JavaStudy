6d23c1b8873f0e6ed3e603ecd367d6411647cc2b
kmarine89 <kmarine89@kongju.ac.kr>
2016-03-21 21:49:09 +0900

    필터 방법에 따른 설명 추가 (메인 메서드 위 주석)

--- src/A/filter/A_FilterMain.java
+++ src/A/filter/A_FilterMain.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class A_FilterMain {
 
+	/**
+	 * Java8 람다 표현식이 포함되지 않은 필터 코드
+	 * 소스코드가 길고 Case by Case의 메서드 구현임
+	 */
 	public static void main(String[] args) {
 		List<Apple> inventory = new ArrayList<>();
 		inventory.add(new Apple("green", 160));
--- src/A/filter/B_FilterMain.java
+++ src/A/filter/B_FilterMain.java
@@ -6,6 +6,11 @@ import java.util.function.Predicate;
 
 public class B_FilterMain {
 
+	/**
+	 * Java8 람다표현식을 이용한 필터 코드
+	 * 소스코드를 짧고 명료하게 구현이 가능 (Case by Case 아님)
+	 * 하지만, 복잡한 필터의 구현은 코드를 더 복잡하게 할 수 있다.
+	 */
 	public static void main(String[] args) {
 		List<Apple> inventory = new ArrayList<>();
 		inventory.add(new Apple("green", 160));
