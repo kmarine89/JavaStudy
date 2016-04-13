5d7ebefcfe2b63b500aac883c9dd2887a116eafd
kmarine89 <kmarine89@daum.net>
2016-04-13 21:32:34 +0900

    데이터구조 배열 리스트 (진행 중)

---
+++ src/datastructure/A/higharray/HighArray.java
@@ -0,0 +1,31 @@
+package datastructure.A.higharray;
+
+public class HighArray {
+
+	private int[] data;
+	private int eElems;
+	
+	public HighArray(int maxSize) {
+		data = new int[maxSize];
+	}
+	
+	public boolean find(int searchValue) {
+		for (int i = 0; i < eElems; i++) {
+			if
+		}
+		return true;
+	}
+	
+	public void insert(int insertValue) {
+		
+	}
+	
+	public void delete(int deleteValue) {
+		
+	}
+	
+	public void display() {
+		
+	}
+	
+}
