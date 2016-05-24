bdfb01f6b3143e957966ae0cf536cd425b53d8c1
kmarine89 <kmarine89@kongju.ac.kr>
2016-05-25 01:54:41 +0900

    미로 찾기 알고리즘 미로 구현 완료

--- src/algorithm/A/Maze.java
+++ src/algorithm/A/Maze.java
@@ -2,8 +2,63 @@ package algorithm.A;
 
 public class Maze {
 
-	public static void main(String[] args) {
-		
+	enum MazeMap {
+		UP(1), RIGHT(2), DOWN(4), LEFT(8);
+		private int value;
+		private MazeMap(int value) {
+			this.value = value;
+		}
+		public int getValue() {
+			return value;
+		}
 	}
-
+	
+	static String shapeStr = "¡¡¦¢¦¬¦Æ¦¢¦¢¦È¦¼¦¬¦Ä¦¬¦º¦Â¦¾¦¸¦»";
+	
+	static char getShape(int x, int y) {
+		int shape = 0;
+		if(map[y][x] != 0) {
+			if(y > 0 && map[y-1][x] > 0)
+				shape += MazeMap.UP.getValue();
+			if(y < map.length-1 && map[y+1][x] > 0)
+				shape += MazeMap.DOWN.getValue();
+			if(x > 0 && map[y][x-1] > 0)
+				shape += MazeMap.LEFT.getValue();
+			if(x < map.length-1 && map[y][x+1] > 0)
+				shape += MazeMap.RIGHT.getValue();
+		}
+		return shapeStr.charAt(shape);
+	}
+	
+	static int[][] map = new int[][] {
+		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+		{0,0,0,0,1,0,0,0,1,0,1,0,0,0,1,0,0,0,0,1},
+		{1,0,1,1,1,1,1,0,1,0,1,1,1,0,1,0,1,1,0,1},
+		{1,0,0,0,0,1,0,0,1,1,1,0,0,0,1,1,1,1,0,1},
+		{1,0,1,1,0,1,0,1,1,0,1,0,1,0,0,0,0,0,0,1},
+		{1,0,1,1,0,1,0,0,0,0,1,0,1,1,1,1,1,1,0,1},
+		{1,0,1,1,1,1,1,0,1,1,1,0,0,1,0,0,0,0,0,1},
+		{1,0,0,0,0,0,1,0,0,0,1,1,1,1,1,1,0,1,0,1},
+		{1,0,1,1,1,0,1,1,1,1,1,0,1,1,0,0,0,1,0,1},
+		{1,0,0,0,1,0,0,0,0,0,0,0,1,1,1,1,1,1,0,1},
+		{1,1,1,0,1,1,1,1,1,1,1,0,1,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,1,0,0,0,0,1,0,1,1,1,1,1,1},
+		{1,0,1,0,1,1,1,1,0,1,1,1,1,0,1,1,0,0,0,1},
+		{1,1,1,1,1,1,1,1,0,1,0,0,0,0,0,0,0,1,0,1},
+		{1,0,0,0,0,0,0,0,0,1,0,1,1,1,1,1,0,1,0,1},
+		{1,1,1,1,1,1,1,1,0,1,0,1,0,0,0,1,0,1,0,1},
+		{1,0,0,0,0,0,0,0,0,1,0,1,0,1,0,1,0,1,0,1},
+		{1,0,1,1,1,1,1,1,1,1,0,1,0,1,1,1,0,1,0,1},
+		{1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,1},
+		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1}
+	};
+	
+	public static void main(String[] args) {
+		for (int y = 0; y < map.length; y++) {
+			for (int x = 0; x < map.length; x++) {
+				System.out.print(getShape(x, y));
+			}
+			System.out.println();
+		}
+	}
 }
