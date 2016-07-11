8662e4c345cdb423b32dc5efa3ddb7693d603819
kmarine89 <kmarine89@kongju.ac.kr>
2016-07-11 23:08:40 +0900

    미로 찾기 Maze.java 주석 추가

--- src/algorithm/A/Maze.java
+++ src/algorithm/A/Maze.java
@@ -1,9 +1,8 @@
 package algorithm.A;
 
-import java.io.IOException;
-
 public class Maze {
 
+	// 방향을 정의 위한 enum >> 쉬프트 해줌으로써 오른쪽으로 돔
 	enum MazeMap {
 		UP(1), RIGHT(2), DOWN(4), LEFT(8), DIR(1);
 		private int value;
@@ -23,8 +22,10 @@ public class Maze {
 		}
 	}
 	
+	// 맵을 그려주기 위한 문자열들...
 	static String shapeStr = "　│━┕││┍┝━┙━┷┑┥┯┿";
 	
+	// 위 문자열을 이용하여 x,y좌표에 맞는 그래프를 그려준다
 	static char getShape(int x, int y) {
 		int shape = 0;
 		if(map[y][x] != 0) {
@@ -42,6 +43,7 @@ public class Maze {
 		return shapeStr.charAt(shape);
 	}
 	
+	// 맵 정보를 다음과 같이 입력함
 	static int[][] map = new int[][] {
 		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
 		{0,0,0,0,1,0,0,0,1,0,1,0,0,0,1,0,0,0,0,1},
@@ -65,19 +67,20 @@ public class Maze {
 		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1}
 	};
 	
+	// 맵 크기를 저장할 변수 객체
 	static Integer x1 = new Integer(18);
 	static Integer y1 = new Integer(18);
 	static MazeMap dir = MazeMap.DIR;
 	
 	public static void main(String[] args) {
 		
-		while(stillInMaze(x1, y1)) {
-			dir.turnRight();
-			while(isWallAhead(x1, y1, dir)) {
-				dir.turnLeft();
+		while(stillInMaze(x1, y1)) {		// 여전히 미로 속인가?
+			dir.turnRight();				// 오른쪽으로 돌아라..
+			while(isWallAhead(x1, y1, dir)) {	// 앞에 벽에 있는가? dir은 앞 방향을 나타냄
+				dir.turnLeft();				// 앞에 벽이 있다면 왼쪽으로 돌아라..
 			}
-			goForward(dir);
-			printMap(x1, y1);
+			goForward(dir);		// 앞으로 한칸 가라..
+			printMap(x1, y1);	// 맵 갱신
 			delay();
 		}
 	}
