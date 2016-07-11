26c56c102349a45b08f01140a68aeabdd012f47d
kmarine89 <kmarine89@kongju.ac.kr>
2016-07-12 01:36:23 +0900

    Calculator 중위식을 하위식으로 변환하는 메서드 생성 완료

---
+++ src/algorithm/A/Calculator.java
@@ -0,0 +1,109 @@
+package algorithm.A;
+
+import java.util.Stack;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		String strInfix = "(10+20*30+40+50-60*70/80)";
+		String strPostfix = toPostFix2(strInfix);
+		System.out.println(strPostfix);
+	}
+
+	// 괄호가 항상 쳐져있는 수식을 후위식으로 변환할 때 사용
+	private static String toPostFix1(String strInfix) {
+		String strPostFix = "";
+		Stack<Character> stack = new Stack<>();
+		
+		for (int i = 0; i < strInfix.length(); i++) 
+		{
+			char ch = strInfix.charAt(i);
+			
+			if(ch == ')') {		// 괄호 닫기를 만나면 연산자를 팝
+				strPostFix += stack.pop();
+				strPostFix += ' ';
+				
+			} else if(ch=='+' || ch=='-' || ch=='*' || ch=='/') {
+				stack.push(ch);
+				
+			} else if(Character.isDigit(ch)) {
+				do {
+					strPostFix += ch;
+					i++;
+					ch = strInfix.charAt(i);
+				} while(Character.isDigit(ch));
+				strPostFix += ' ';
+				i--;
+			}
+			
+		}
+		return strPostFix;
+	}
+
+	// 괄호에 상관없고 연산자 우선순위를 따져가며 알아서 잘 후위식으로 변환해주는 메서드 
+	private static String toPostFix2(String strInfix) {
+		String strPostfix = "";
+		Stack<Character> stack = new Stack<>();
+		
+		for (int i = 0; i < strInfix.length(); i++) {
+			char c = strInfix.charAt(i);
+			
+			if(c == '(') {
+				stack.push(c);
+				
+			} else if(c == ')') {
+				while(!stack.isEmpty() && stack.peek() != '(') {
+					strPostfix += stack.pop();
+					strPostfix += ' ';
+				}
+				stack.pop();
+				
+			} else if(c=='+' || c=='-' || c=='*' || c=='/') {
+				while(!stack.isEmpty() && getPrecedence(stack.peek()) >= getPrecedence(c)) {
+					strPostfix += stack.pop();
+					strPostfix += ' ';
+				}
+				stack.push(c);
+				
+			} else if(Character.isDigit(c)) {
+				do {
+					strPostfix += c;
+					c = strInfix.charAt(++i);
+				} while(Character.isDigit(c));
+				strPostfix += ' ';
+				i--;
+				
+			}
+		}
+		
+		while(!stack.isEmpty()) {
+			strPostfix += stack.pop();
+			strPostfix += ' ';
+		}
+		
+		return strPostfix;
+	}
+
+	private static int getPrecedence(Character c) {
+		if(c == '(' || c == ')') {
+			return 1;
+		} else if(c == '+' || c == '-') {
+			return 2;
+		} else if(c == '*' || c == '/') {
+			return 3;
+		}
+		return 0;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
